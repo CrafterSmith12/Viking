1402328b614d396de75125564e1df90b20cb2467
Wade <CrafterSmith12@users.noreply.github.com>
2016-02-10 16:20:04 -0500

    Wiped except for some variables
    code will be moved later for organization.

--- Viking/src/viking/Player.java
+++ Viking/src/viking/Player.java
@@ -1,119 +1,42 @@
 package viking;
 
 import java.util.Scanner;
-import static viking.Engine.*;
 
 public class Player 
 {
+	public static int coins1 = 10;
+	public static int coins2 = 10;
+	public static int coins3 = 10;
+	public static int coins4 = 10;
+	        
+	public static boolean p1H = false;
+	public static boolean p2H = false;
+	public static boolean p3H = false;
+	public static boolean p4H = false;
+	        
+	public static boolean p1R = false;
+	public static boolean p2R = false;
+	public static boolean p3R = false;
+	public static boolean p4R = false;
+	        
+	public static boolean p1G = false;
+	public static boolean p2G = false;
+	public static boolean p3G = false;
+	public static boolean p4G = false;
+	
+	public static boolean p1T = false;
+	public static boolean p2T = false;
+	public static boolean p3T = false;
+	public static boolean p4T = false;
+	        
+	public static String p1Raiding = "x";
+	public static String p2Raiding = "x";
+	public static String p3Raiding = "x";
+	public static String p4Raiding = "x";
+	        
+	public static String turn = "x";
 	void act()
 	{
-		int coins = 10;
-		
-		Scanner sc = new Scanner(System.in);
-		while (players >= 2 || players <= 4)
-		{
-			Engine.enteredAmountOfPlayers = true;
-
-			if (Engine.enteredAmountOfPlayers == true && !Engine.sure) 
-			{
-				System.out.println("Are you sure you want " + players + " servants, my lord? (Y or N)");
-				Engine.areYouSure = sc.next();
-
-				if(Engine.areYouSure.equalsIgnoreCase("n"))
-				{
-					Engine.sure = false;
-					Engine.enteredAmountOfPlayers = false;
-				}
-				else if(Engine.areYouSure.equalsIgnoreCase("y"))
-				{
-					Engine.sure = true;
-					Engine.enteredAmountOfPlayers = true;
-					System.out.println("You have chosen " + players + " servants, my lord.");
-					Engine.done = true;
-				}
-				
-				if(Engine.players == 2 && Engine.done == true)
-				{
-					String player1 = "Player 1";
-					String player2 = "Player 2";
-
-					System.out.println("Choose the name of " + player1);
-					player1 = sc.next();
-					System.out.println("Player 1 is now called: " + player1);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player2);
-					player2 = sc.next();
-					System.out.println("Player 2 is now called: " + player2);
-					System.out.println("\n");
-
-				}
-				else if(Engine.players == 3 && Engine.done == true)
-				{
-					String player1 = "Player 1";
-					String player2 = "Player 2";
-					String player3 = "Player 3";
-
-					System.out.println("Choose the name of " + player1);
-					player1 = sc.next();
-					System.out.println("Player 1 is now called: " + player1);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player2);
-					player2 = sc.next();
-					System.out.println("Player 2 is now called: " + player2);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player3);
-					player3 = sc.next();
-					System.out.println("Player 3 is now called: " + player3);
-					System.out.println("\n");
-				}
-				else if(Engine.players == 4 && Engine.done == true)
-				{
-					String player1 = "Player 1";
-					String player2 = "Player 2";
-					String player3 = "Player 3";
-					String player4 = "Player 4";
-
-					System.out.println("Choose the name of " + player1);
-					player1 = sc.next();
-					System.out.println("Player 1 is now called: " + player1);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player2);
-					player2 = sc.next();
-					System.out.println("Player 2 is now called: " + player2);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player3);
-					player3 = sc.next();
-					System.out.println("Player 3 is now called: " + player3);
-					System.out.println("\n");
-
-					System.out.println("Choose the name of " + player4);
-					player4 = sc.next();
-					System.out.println("Player 4 is now called: " + player4);
-					System.out.println("\n");
-				}
-				if(Engine.enteredAmountOfPlayers == false)
-				{
-
-					if(Engine.areYouSure.equalsIgnoreCase("n"))
-					{
-						System.out.println("Choose players (2-4)");
-						players = sc.nextInt();
-					}
-					if(players < 2 || players > 4)
-					{
-						Engine.enteredAmountOfPlayers = false;
-						System.out.println("That is an invalid amount of servants, my lord!");
-						System.out.println("Choose players (2-4)");
-						players = sc.nextInt();
-					}
-				}
-			}
-		}
-	}				
+         
+	}
 }
-
