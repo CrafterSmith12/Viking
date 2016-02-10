aa97858dd6073f7d80149cee1129224437a39ccf
Wade <CrafterSmith12@users.noreply.github.com>
2016-02-10 16:18:54 -0500

    Made some of it runnable
    still buggy and still needs to be worked on / moved.

--- Viking/src/viking/Engine.java
+++ Viking/src/viking/Engine.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Engine 
 {
-	public static int players = 0;
-	public static boolean done = false;
-        public static boolean enteredAmountOfPlayers = false;
-	public static boolean sure = false;
-        public static String areYouSure = "x";
+	static int players = 0;
+	static String areYouSure = "x";
+	static boolean done = false;
+	static boolean enteredAmountOfPlayers = false;
 
-	//	public static void main(String[] args) 
-	//	{
 	void act()
 	{
+		boolean sure = false;
+
 		Scanner sc = new Scanner(System.in);
+		Player p = new Player();
 
 		System.out.println("Welcome, my lord. Choose how many servants you desire.");
 		System.out.println("Choose players (2-4)");
@@ -62,7 +62,126 @@ public class Engine
 					System.out.println("Choose the name of " + player2);
 					player2 = sc.next();
 					System.out.println("Player 2 is now called: " + player2);
-					System.out.println("\n");
+					System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+					p.p1T = true;
+					if(p.p1T == true)
+					{
+                    System.out.println("It's " + player1 + "'s turn.");
+                    System.out.println("\n");
+                    System.out.println("You have " + p.coins1 + " coins.");
+                    System.out.println(player2 + " has " + p.coins2 + " coins.");
+                    System.out.println("Please choose to either: [H]arvest, [R]aid, or [G]uard");
+                    p.turn = sc.next();
+                    if(p.turn.equalsIgnoreCase("h"))
+                    {
+                    p.p1H = true;
+                    p.p1T = false;
+                    p.p2T = true;
+                    } else if(p.turn.equalsIgnoreCase("r"))
+                    {
+                        System.out.println("Who would you like to raid?");
+                        System.out.println("- " + player2);
+                        if(p.turn.equalsIgnoreCase(player2))
+                        {
+                         p.p1Raiding = player2;
+                         p.p1R = true;
+                         p.p1T = false;
+                         p.p2T = true;
+                        } else if(p.turn.equalsIgnoreCase(player1))
+                        {
+                         System.out.println("You cannot raid yourself!");
+                        }
+                    } else if(p.turn.equalsIgnoreCase("g"))
+                    {
+                        p.p1G = true;
+                        p.p1T = false;
+                        p.p2T = true;
+                    }
+					}
+					if(p.p2T == true)
+					{
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                    System.out.println("It's " + player2 + "'s turn.");
+                    System.out.println("\n");
+                    System.out.println("You have " + p.coins2 + " coins.");
+                    System.out.println(player1 + " has " + p.coins1 + " coins.");
+                    System.out.println("Please choose to either: [H]arvest, [R]aid, or [G]uard");
+                    p.turn = sc.next();
+                    if(p.turn.equalsIgnoreCase("h"))
+                    {
+                    p.p2H = true;
+                    p.p2T = false;
+                    } else if(p.turn.equalsIgnoreCase("r"))
+                    {
+                        System.out.println("Who would you like to raid?");
+                        System.out.println("- " + player1);
+                        p.p1Raiding = sc.next();
+                        if(p.p1Raiding.equalsIgnoreCase(player1))
+                        {
+                         p.p2Raiding = player1;
+                         p.p2R = true;
+                         p.p2T = false;
+                        }
+                    } else if(p.turn.equalsIgnoreCase("g"))
+                    {
+                        p.p2G = true;
+                        p.p2T = false;
+                    }
+					}
+                    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+                    System.out.println("Results:");
+                    System.out.println("\n");
+                    if(p.p1H == true && p.p2Raiding != player1)
+                    {
+                        p.coins1 = p.coins1 + 4;
+                        System.out.println(player1 + " harvested and gained 4 coins!");
+                        p.p1H = false;
+                    } else if(p.p1G == true && p.p2Raiding == player1)
+                    {
+                     System.out.println(player1 + " has guarded while " + player2 + " tried to raid!");
+                     System.out.println(player1 + " lost 1 coin and " + player2 + " lost 5!");
+                     p.p1G = false;
+                     p.p2Raiding = "x";
+                    } else if(p.p1G == true && p.p2Raiding != player1)
+                    {
+                     System.out.println(player1 + " guarded but no one attacked!");
+                     System.out.println(player1 + " lost 1 coin.");
+                     p.coins1 = p.coins1 - 1;
+                     p.p1G = false;
+                    } else if(p.p1H == true && p.p2Raiding == player1 && p.p2R == true)
+                    {
+                        System.out.println(player1 + " went harvesting but got raided by " + player2 + "!");
+                        System.out.println("Nothing happens.");
+                        p.p2Raiding = "x";
+                        p.p1H = false;
+                        p.p2R = false;
+                    } else if(p.p1R == true && p.p1Raiding == player2 && p.p2G == false && p.p2H == false)
+                    {
+                        System.out.println(player1 + " raided " + player2 + "while wasn't guarding!");
+                        System.out.println(player1 + " gained 3 coins.");
+                        p.coins1 = p.coins1 + 3;
+                        System.out.println(player2 + " lost 3 coins.");
+                        p.coins2 = p.coins2 - 3;
+                        p.p1R = false;
+                        p.p1Raiding = "x";
+                    } else if(p.p1R == true && p.p1Raiding == player2 && p.p2G == true && p.p2H == false)
+                    {
+                        System.out.println(player1 + " raided while " + player2 + " was guarding!");
+                        System.out.println(player1 + " lost 5 coins.");
+                        p.coins1 = p.coins1 - 5;
+                        System.out.println(player1 + " lost 1 coin.");
+                        p.coins1 = p.coins1 - 1;
+                        p.p1R = false;
+                        p.p2G = false;
+                        p.p1Raiding = "x";
+                    } else if(p.p1R == true && p.p1Raiding == player2 && p.p2G == false && p.p2H == true)
+                    {
+                        System.out.println(player2 + " went harvesting but got raided by " + player1 + "!");
+                        System.out.println("Nothing happens.");
+                        p.p1Raiding = "x";
+                        p.p2H = false;
+                        p.p1R = false;
+                    }
 
 				}
 				else if(Engine.players == 3 && Engine.done == true)
@@ -133,3 +252,5 @@ public class Engine
 		}
 	}
 }
+
+
